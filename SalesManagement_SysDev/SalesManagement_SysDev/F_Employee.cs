bc8e2464b021c009355a367b628df6b6e2f9dc21
2C31 <c1133@oic.jp>
2022-10-20 14:51:35 +0900

    client

---
+++ SalesManagement_SysDev/SalesManagement_SysDev/F_Employee.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace system_1_
+{
+    public partial class member : Form
+    {
+        public member()
+        {
+            InitializeComponent();
+        }
+
+        ///////////////////////////////
+        //メソッド名：fncButtonEnable()
+        //引　数   ：int型：chk　（社員IDの入力状況）
+        //戻り値   ：なし
+        //機　能   ：社員IDの入力状況に応じてボタンの
+        //          ：enableプロパティの設定
+        ///////////////////////////////
+        private void fncButtonEnable(int chk)
+        {
+            //chkが１ならば、社員IDが空、２ならば有
+            if (chk == 1)
+            {
+                //Enableは、コントロールの使用可否
+                buttonRegist.Enabled = false;
+                buttonSearch.Enabled = true;
+                buttonAll.Enabled = true;
+                buttonClear.Enabled = true;
+                buttonHiddenList.Enabled = true;
+                buttonHidden.Enabled = true;
+                buttonUpdate.Enabled = false;
+            }
+            else if (chk == 2)
+            {
+                buttonRegist.Enabled = true;
+                buttonSearch.Enabled = true;
+                buttonAll.Enabled = true;
+                buttonClear.Enabled = true;
+                buttonHiddenList.Enabled = true;
+                buttonHidden.Enabled = true;
+                buttonUpdate.Enabled = true;
+            }
+
+        }
+        ///////////////////////////////
+        //メソッド名：fncInputCheck()
+        //引　数   ：int型：追加又は更新を区別
+        //戻り値   ：true(エラーなし） Or false（エラーあり）
+        //機　能   ：入力データのチェック
+        //          ：引数が1は追加処理、2は更新処理
+        ///////////////////////////////
+        private bool fncInputCheck(int chk)
+        {
+            DialogResult result;
+            //追加時のみ社員IDの文字数チェック
+            if (chk == 1 && Encoding.GetEncoding("Shift_JIS").GetByteCount(textBoxEmID.Text) != 5)
+            {
+                //文字数エラーメッセージ表示
+                result = msg.MsgDsp("M30030");
+                textBoxEmID.Focus();
+                return false;
+            }
+            //スタッフ名の入力データ有無チェック
+            if ((textBoxEmName.Text).Trim() == "" || textBoxEmName.Text == null)
+            {
+                //入力無しエラーメッセージの表示
+                result = msg.MsgDsp("M30009");
+                textBoxEmName.Focus();
+                return false;
+            }
+            //
+            //スタッフ名の文字数チェック
+            if (Encoding.GetEncoding("Shift_JIS").GetByteCount(textBoxEmName.Text) > 50)
+            {
+                //文字数エラーメッセージの表示
+                result = msg.MsgDsp("M30010");
+                textBoxEmName.Focus();
+                return false;
+            }
+            //店舗名の入力データ有無チェック
+            if ((comboBoxStore.Text).Trim() == "" || comboBoxStore.Text == null)
+            {
+                //入力無しエラーメッセージの表示
+                result = msg.MsgDsp("M30013");
+                comboBoxStore.Focus();
+                return false;
+            }
+            //役職名の入力データ有無チェック
+            if ((comboBoxPosition.Text).Trim() == "" || comboBoxPosition.Text == null)
+            {
+                //入力無しエラーメッセージの表示
+                result = msg.MsgDsp("M30014");
+                comboBoxPosition.Focus();
+                return false;
+            }
+            //部署名の入力データ有無チェック
+            if ((comboBoxDivision.Text).Trim() == "" || comboBoxDivision.Text == null)
+            {
+                //入力無しエラーメッセージの表示
+                result = msg.MsgDsp("M30015");
+                comboBoxDivision.Focus();
+                return false;
+            }
+            //ログインIDの入力データ有無チェック
+            if ((textBoxLoginID.Text).Trim() == "" || textBoxLoginID.Text == null)
+            {
+                //入力無しエラーメッセージの表示
+                result = msg.MsgDsp("M30016");
+                textBoxLoginID.Focus();
+                return false;
+            }
+            //ログインIDの文字数チェック
+            if (Encoding.GetEncoding("Shift_JIS").GetByteCount(textBoxLoginID.Text) > 20)
+            {
+                //文字数エラーメッセージの表示
+                result = msg.MsgDsp("M30017");
+                textBoxLoginID.Focus();
+                return false;
+            }
+            //追加処理の場合のみログインPWの入力データ有無チェック
+            if (chk == 1 && (textBoxLoginPW.Text).Trim() == "" || textBoxLoginPW.Text == null)
+            {
+                //入力無しエラーメッセージの表示
+                result = msg.MsgDsp("M30018");
+                textBoxLoginPW.Focus();
+                return false;
+            }
+            //ログインPWの文字数チェック
+            if (Encoding.GetEncoding("Shift_JIS").GetByteCount(textBoxLoginPW.Text) > 20)
+            {
+                //文字数エラーメッセージの表示
+                result = msg.MsgDsp("M30019");
+                textBoxLoginPW.Focus();
+                return false;
+            }
+            //郵便番号の文字数チェック
+            if (Encoding.GetEncoding("Shift_JIS").GetByteCount(textBoxPostal.Text) > 7)
+            {
+                //文字数エラーメッセージの表示
+                result = msg.MsgDsp("M30020");
+                textBoxPostal.Focus();
+                return false;
+            }
+            //住所の文字数チェック
+            if (Encoding.GetEncoding("Shift_JIS").GetByteCount(textBoxAddress.Text) > 50)
+            {
+                //文字数エラーメッセージの表示
+                result = msg.MsgDsp("M30021");
+                textBoxAddress.Focus();
+                return false;
+            }
+            //住所カナの文字数チェック
+            if (Encoding.GetEncoding("Shift_JIS").GetByteCount(textBoxAddressKana.Text) > 50)
+            {
+                //文字数エラーメッセージの表示
+                result = msg.MsgDsp("M30022");
+                textBoxAddressKana.Focus();
+                return false;
+            }
+            //電話番号の文字数チェック
+            if (Encoding.GetEncoding("Shift_JIS").GetByteCount(textBoxTel.Text) > 12)
+            {
+                //文字数エラーメッセージの表示
+                result = msg.MsgDsp("M30023");
+                textBoxTel.Focus();
+                return false;
+            }
+            //携帯番号の文字数チェック
+            if (Encoding.GetEncoding("Shift_JIS").GetByteCount(textBoxMobileTel.Text) > 12)
+            {
+                //文字数エラーメッセージの表示
+                result = msg.MsgDsp("M30024");
+                textBoxMobileTel.Focus();
+                return false;
+            }
+            //メールアドレスの文字数チェック
+            if (Encoding.GetEncoding("Shift_JIS").GetByteCount(textBoxMail.Text) > 30)
+            {
+                //文字数エラーメッセージの表示
+                result = msg.MsgDsp("M30025");
+                textBoxMail.Focus();
+                return false;
+            }
+            //備考の文字数チェック
+            if (Encoding.GetEncoding("Shift_JIS").GetByteCount(textBoxComments.Text) > 80)
+            {
+                //文字数エラーメッセージの表示
+                result = msg.MsgDsp("M30026");
+                textBoxComments.Focus();
+                return false;
+            }
+            return true;
+        }
+
+        ///////////////////////////////
+        //メソッド名：fncExistenceIdCheck()
+        //引　数   ：int型：追加又は更新を区別
+        //戻り値   ：true(データあり） Or false（データなし）
+        //機　能   ：スタッフ情報の存在チェック
+        //         ：引数が1は追加、更新処理、2は検索処理
+        ///////////////////////////////
+        private bool fncExistenceIdCheck(int chk)
+        {
+            bool flg = false;
+            try
+            {
+                var context = new SalesContext();
+                //追加・更新処理の場合（引数１）、社員IDが存在するか
+                //検索処理の場合（引数２）、社員ID、スタッフ名、スタッフ名カナ、店舗名で部分一致でデータが存在するか
+                if (chk == 1)
+                    flg = context.M_Ems.Any(x => x.EmID.Contains(textBoxEmID.Text));
+                else if (chk == 2)
+                    flg = context.M_Ems.Any(x => x.EmID.Contains(textBoxEmID.Text)
+                                            && x.EmName.Contains(textBoxEmName.Text)
+                                            && x.EmNameKana.Contains(textBoxEmNameKana.Text)
+                                            && x.StoreCD.Contains(comboBoxStore.SelectedValue.ToString()));
+                context.Dispose();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            return flg;
+        }
+
+        ///////////////////////////////
+        //メソッド名：fncPasswordHash()
+        //引　数   ：なし
+        //戻り値   ：ハッシュ化されたパスワード
+        //機　能   ：パスワードをハッシュ化
+        ///////////////////////////////
+        private string fncPasswordHash()
+        {
+            const string Key = @"pbkdf2s@ltkeyt0h@sh";
+            string salt = Key + textBoxLoginID.Text;
+            var pw = "";
+            var encoder = new UTF8Encoding();
+
+            var b = new Rfc2898DeriveBytes(textBoxLoginPW.Text, encoder.GetBytes(salt), 10000);
+            var k = b.GetBytes(32);
+            pw = Convert.ToBase64String(k);
+            return pw;
+        }
+    }
+}
---
+++ SalesManagement_SysDev/SalesManagement_SysDev/F_Employee.designer.cs
@@ -0,0 +1,334 @@
+﻿
+namespace system_1_
+{
+    partial class member
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonRegist = new System.Windows.Forms.Button();
+            this.buttonUpdate = new System.Windows.Forms.Button();
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.buttonAll = new System.Windows.Forms.Button();
+            this.buttonHiddenList = new System.Windows.Forms.Button();
+            this.buttonHidden = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.textBoxEmID = new System.Windows.Forms.TextBox();
+            this.textBoxSoID = new System.Windows.Forms.TextBox();
+            this.textBoxEmName = new System.Windows.Forms.TextBox();
+            this.textBoxPoID = new System.Windows.Forms.TextBox();
+            this.textBoxEmPassword = new System.Windows.Forms.TextBox();
+            this.textBoxEmPhone = new System.Windows.Forms.TextBox();
+            this.textBoxEmHidden = new System.Windows.Forms.TextBox();
+            this.labelEmID = new System.Windows.Forms.Label();
+            this.labelEmName = new System.Windows.Forms.Label();
+            this.labelSoID = new System.Windows.Forms.Label();
+            this.labelPoID = new System.Windows.Forms.Label();
+            this.labelEmHiredate = new System.Windows.Forms.Label();
+            this.labelEmPassword = new System.Windows.Forms.Label();
+            this.labelEmPhone = new System.Windows.Forms.Label();
+            this.labelEmFlag = new System.Windows.Forms.Label();
+            this.labelEmHidden = new System.Windows.Forms.Label();
+            this.checkBoxEmFlag = new System.Windows.Forms.CheckBox();
+            this.dateTimePickerEmHiredate = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // buttonRegist
+            // 
+            this.buttonRegist.Location = new System.Drawing.Point(86, 26);
+            this.buttonRegist.Name = "buttonRegist";
+            this.buttonRegist.Size = new System.Drawing.Size(75, 35);
+            this.buttonRegist.TabIndex = 0;
+            this.buttonRegist.Text = "登録";
+            this.buttonRegist.UseVisualStyleBackColor = true;
+            // 
+            // buttonUpdate
+            // 
+            this.buttonUpdate.Location = new System.Drawing.Point(167, 26);
+            this.buttonUpdate.Name = "buttonUpdate";
+            this.buttonUpdate.Size = new System.Drawing.Size(75, 35);
+            this.buttonUpdate.TabIndex = 1;
+            this.buttonUpdate.Text = "更新";
+            this.buttonUpdate.UseVisualStyleBackColor = true;
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.Location = new System.Drawing.Point(248, 26);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(75, 35);
+            this.buttonSearch.TabIndex = 2;
+            this.buttonSearch.Text = "検索";
+            this.buttonSearch.UseVisualStyleBackColor = true;
+            // 
+            // buttonAll
+            // 
+            this.buttonAll.Location = new System.Drawing.Point(329, 26);
+            this.buttonAll.Name = "buttonAll";
+            this.buttonAll.Size = new System.Drawing.Size(112, 35);
+            this.buttonAll.TabIndex = 3;
+            this.buttonAll.Text = "一覧表示";
+            this.buttonAll.UseVisualStyleBackColor = true;
+            // 
+            // buttonHiddenList
+            // 
+            this.buttonHiddenList.Location = new System.Drawing.Point(447, 26);
+            this.buttonHiddenList.Name = "buttonHiddenList";
+            this.buttonHiddenList.Size = new System.Drawing.Size(112, 35);
+            this.buttonHiddenList.TabIndex = 4;
+            this.buttonHiddenList.Text = "非表示リスト";
+            this.buttonHiddenList.UseVisualStyleBackColor = true;
+            // 
+            // buttonHidden
+            // 
+            this.buttonHidden.Location = new System.Drawing.Point(565, 26);
+            this.buttonHidden.Name = "buttonHidden";
+            this.buttonHidden.Size = new System.Drawing.Size(75, 35);
+            this.buttonHidden.TabIndex = 5;
+            this.buttonHidden.Text = "非表示";
+            this.buttonHidden.UseVisualStyleBackColor = true;
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(662, 26);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(104, 35);
+            this.buttonClear.TabIndex = 6;
+            this.buttonClear.Text = "入力クリア";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            // 
+            // textBoxEmID
+            // 
+            this.textBoxEmID.Location = new System.Drawing.Point(129, 126);
+            this.textBoxEmID.Name = "textBoxEmID";
+            this.textBoxEmID.Size = new System.Drawing.Size(100, 25);
+            this.textBoxEmID.TabIndex = 7;
+            // 
+            // textBoxSoID
+            // 
+            this.textBoxSoID.Location = new System.Drawing.Point(565, 126);
+            this.textBoxSoID.Name = "textBoxSoID";
+            this.textBoxSoID.Size = new System.Drawing.Size(100, 25);
+            this.textBoxSoID.TabIndex = 8;
+            // 
+            // textBoxEmName
+            // 
+            this.textBoxEmName.Location = new System.Drawing.Point(316, 126);
+            this.textBoxEmName.Name = "textBoxEmName";
+            this.textBoxEmName.Size = new System.Drawing.Size(140, 25);
+            this.textBoxEmName.TabIndex = 10;
+            // 
+            // textBoxPoID
+            // 
+            this.textBoxPoID.Location = new System.Drawing.Point(129, 188);
+            this.textBoxPoID.Name = "textBoxPoID";
+            this.textBoxPoID.Size = new System.Drawing.Size(100, 25);
+            this.textBoxPoID.TabIndex = 11;
+            // 
+            // textBoxEmPassword
+            // 
+            this.textBoxEmPassword.Location = new System.Drawing.Point(565, 241);
+            this.textBoxEmPassword.Name = "textBoxEmPassword";
+            this.textBoxEmPassword.Size = new System.Drawing.Size(100, 25);
+            this.textBoxEmPassword.TabIndex = 12;
+            // 
+            // textBoxEmPhone
+            // 
+            this.textBoxEmPhone.Location = new System.Drawing.Point(129, 245);
+            this.textBoxEmPhone.Name = "textBoxEmPhone";
+            this.textBoxEmPhone.Size = new System.Drawing.Size(100, 25);
+            this.textBoxEmPhone.TabIndex = 13;
+            // 
+            // textBoxEmHidden
+            // 
+            this.textBoxEmHidden.Location = new System.Drawing.Point(50, 330);
+            this.textBoxEmHidden.Multiline = true;
+            this.textBoxEmHidden.Name = "textBoxEmHidden";
+            this.textBoxEmHidden.Size = new System.Drawing.Size(691, 130);
+            this.textBoxEmHidden.TabIndex = 15;
+            // 
+            // labelEmID
+            // 
+            this.labelEmID.AutoSize = true;
+            this.labelEmID.Location = new System.Drawing.Point(47, 129);
+            this.labelEmID.Name = "labelEmID";
+            this.labelEmID.Size = new System.Drawing.Size(60, 18);
+            this.labelEmID.TabIndex = 16;
+            this.labelEmID.Text = "社員ID";
+            // 
+            // labelEmName
+            // 
+            this.labelEmName.AutoSize = true;
+            this.labelEmName.Location = new System.Drawing.Point(248, 129);
+            this.labelEmName.Name = "labelEmName";
+            this.labelEmName.Size = new System.Drawing.Size(62, 18);
+            this.labelEmName.TabIndex = 17;
+            this.labelEmName.Text = "社員名";
+            // 
+            // labelSoID
+            // 
+            this.labelSoID.AutoSize = true;
+            this.labelSoID.Location = new System.Drawing.Point(481, 129);
+            this.labelSoID.Name = "labelSoID";
+            this.labelSoID.Size = new System.Drawing.Size(78, 18);
+            this.labelSoID.TabIndex = 18;
+            this.labelSoID.Text = "営業所ID";
+            // 
+            // labelPoID
+            // 
+            this.labelPoID.AutoSize = true;
+            this.labelPoID.Location = new System.Drawing.Point(47, 191);
+            this.labelPoID.Name = "labelPoID";
+            this.labelPoID.Size = new System.Drawing.Size(60, 18);
+            this.labelPoID.TabIndex = 19;
+            this.labelPoID.Text = "役職ID";
+            // 
+            // labelEmHiredate
+            // 
+            this.labelEmHiredate.AutoSize = true;
+            this.labelEmHiredate.Location = new System.Drawing.Point(248, 191);
+            this.labelEmHiredate.Name = "labelEmHiredate";
+            this.labelEmHiredate.Size = new System.Drawing.Size(98, 18);
+            this.labelEmHiredate.TabIndex = 20;
+            this.labelEmHiredate.Text = "入社年月日";
+            // 
+            // labelEmPassword
+            // 
+            this.labelEmPassword.AutoSize = true;
+            this.labelEmPassword.Location = new System.Drawing.Point(480, 248);
+            this.labelEmPassword.Name = "labelEmPassword";
+            this.labelEmPassword.Size = new System.Drawing.Size(79, 18);
+            this.labelEmPassword.TabIndex = 21;
+            this.labelEmPassword.Text = "パスワード";
+            // 
+            // labelEmPhone
+            // 
+            this.labelEmPhone.AutoSize = true;
+            this.labelEmPhone.Location = new System.Drawing.Point(47, 248);
+            this.labelEmPhone.Name = "labelEmPhone";
+            this.labelEmPhone.Size = new System.Drawing.Size(80, 18);
+            this.labelEmPhone.TabIndex = 22;
+            this.labelEmPhone.Text = "電話番号";
+            // 
+            // labelEmFlag
+            // 
+            this.labelEmFlag.AutoSize = true;
+            this.labelEmFlag.Location = new System.Drawing.Point(248, 248);
+            this.labelEmFlag.Name = "labelEmFlag";
+            this.labelEmFlag.Size = new System.Drawing.Size(118, 18);
+            this.labelEmFlag.TabIndex = 23;
+            this.labelEmFlag.Text = "社員管理フラグ";
+            // 
+            // labelEmHidden
+            // 
+            this.labelEmHidden.AutoSize = true;
+            this.labelEmHidden.Location = new System.Drawing.Point(47, 309);
+            this.labelEmHidden.Name = "labelEmHidden";
+            this.labelEmHidden.Size = new System.Drawing.Size(98, 18);
+            this.labelEmHidden.TabIndex = 24;
+            this.labelEmHidden.Text = "非表示理由";
+            // 
+            // checkBoxEmFlag
+            // 
+            this.checkBoxEmFlag.AutoSize = true;
+            this.checkBoxEmFlag.Location = new System.Drawing.Point(382, 247);
+            this.checkBoxEmFlag.Name = "checkBoxEmFlag";
+            this.checkBoxEmFlag.Size = new System.Drawing.Size(22, 21);
+            this.checkBoxEmFlag.TabIndex = 25;
+            this.checkBoxEmFlag.UseVisualStyleBackColor = true;
+            // 
+            // dateTimePickerEmHiredate
+            // 
+            this.dateTimePickerEmHiredate.Location = new System.Drawing.Point(352, 188);
+            this.dateTimePickerEmHiredate.Name = "dateTimePickerEmHiredate";
+            this.dateTimePickerEmHiredate.Size = new System.Drawing.Size(200, 25);
+            this.dateTimePickerEmHiredate.TabIndex = 26;
+            // 
+            // member
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 487);
+            this.Controls.Add(this.dateTimePickerEmHiredate);
+            this.Controls.Add(this.checkBoxEmFlag);
+            this.Controls.Add(this.labelEmHidden);
+            this.Controls.Add(this.labelEmFlag);
+            this.Controls.Add(this.labelEmPhone);
+            this.Controls.Add(this.labelEmPassword);
+            this.Controls.Add(this.labelEmHiredate);
+            this.Controls.Add(this.labelPoID);
+            this.Controls.Add(this.labelSoID);
+            this.Controls.Add(this.labelEmName);
+            this.Controls.Add(this.labelEmID);
+            this.Controls.Add(this.textBoxEmHidden);
+            this.Controls.Add(this.textBoxEmPhone);
+            this.Controls.Add(this.textBoxEmPassword);
+            this.Controls.Add(this.textBoxPoID);
+            this.Controls.Add(this.textBoxEmName);
+            this.Controls.Add(this.textBoxSoID);
+            this.Controls.Add(this.textBoxEmID);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.buttonHidden);
+            this.Controls.Add(this.buttonHiddenList);
+            this.Controls.Add(this.buttonAll);
+            this.Controls.Add(this.buttonSearch);
+            this.Controls.Add(this.buttonUpdate);
+            this.Controls.Add(this.buttonRegist);
+            this.Name = "member";
+            this.Text = "社員管理";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonRegist;
+        private System.Windows.Forms.Button buttonUpdate;
+        private System.Windows.Forms.Button buttonSearch;
+        private System.Windows.Forms.Button buttonAll;
+        private System.Windows.Forms.Button buttonHiddenList;
+        private System.Windows.Forms.Button buttonHidden;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.TextBox textBoxEmID;
+        private System.Windows.Forms.TextBox textBoxSoID;
+        private System.Windows.Forms.TextBox textBoxEmName;
+        private System.Windows.Forms.TextBox textBoxPoID;
+        private System.Windows.Forms.TextBox textBoxEmPassword;
+        private System.Windows.Forms.TextBox textBoxEmPhone;
+        private System.Windows.Forms.TextBox textBoxEmHidden;
+        private System.Windows.Forms.Label labelEmID;
+        private System.Windows.Forms.Label labelEmName;
+        private System.Windows.Forms.Label labelSoID;
+        private System.Windows.Forms.Label labelPoID;
+        private System.Windows.Forms.Label labelEmHiredate;
+        private System.Windows.Forms.Label labelEmPassword;
+        private System.Windows.Forms.Label labelEmPhone;
+        private System.Windows.Forms.Label labelEmFlag;
+        private System.Windows.Forms.Label labelEmHidden;
+        private System.Windows.Forms.CheckBox checkBoxEmFlag;
+        private System.Windows.Forms.DateTimePicker dateTimePickerEmHiredate;
+    }
+}
